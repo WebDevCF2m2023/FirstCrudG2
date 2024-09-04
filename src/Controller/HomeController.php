874d9ea18919948819715b4a067fa399e1a96667
WebDevCF2m <gitweb@cf2m.be>
2024-09-04 15:52:02 +0200

    crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+# gestionnaire de nos entités
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Article;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        // on récupère tous les articles
+        $articles = $em->getRepository(Article::class)->findAll();
         return $this->render('home/index.html.twig', [
             // variables envoyées à twig
             'titre' => 'Homepage',
+            'articles' => $articles,
         ]);
     }
 }
